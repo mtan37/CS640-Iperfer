991e89e7c3fee62a5cc5152f2e137e946c6c5119
Marvin <marvin.tan@wisc.edu>
2021-02-13 16:32:54 -0600

    server class finish

--- src/main/IperferClient.java
+++ src/main/IperferClient.java
@@ -1,26 +1,27 @@
-package src;
+package main;
 
 import java.io.*;
-import java.net.*
+import java.net.*;
 
 public class IperferClient extends Iperfer {
 	private String serverName;
 	private int serverPort;
 	private Socket conn;
 	private int time;
-	
-	IperferClient(String serverName, int serverPort, int runtime){
+
+	IperferClient(String serverName, int serverPort, int runtime) {
 		this.serverName = serverName;
 		this.serverPort = serverPort;
 		this.time = runtime;
 	}
-	
+
 	@Override
 	/**
 	 * Runs an iperfer client which sends data to a server for a determined amount of time
 	 */
 	void run() {
 		// TODO Auto-generated method stub
+		/*
 		long start = System.nanoTime();
 		try {
 			conn = new Socket(serverName, serverPort);
@@ -28,16 +29,22 @@ public class IperferClient extends Iperfer {
 			while(time > 0) {
 				
 			}
-		}
+		}*/
 		
 		
 	}
-	
+
 	/**
 	 * Sends a 1000 byte chunk of data to the server
 	 */
 	private void sendData() {
-		//TODO
+		// TODO
+	}
+
+	@Override
+	void printSummary(double DataKB, double rate) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
